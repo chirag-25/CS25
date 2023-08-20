bf99e2dbe79c1a16fe02d106411e6d6f0ad35b86
Chirag Sarda <chirag.sarda@iitgn.ac.in>
2023-08-20 19:17:33 +0530

    about us, experience, skills added

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const categoryButtons = document.querySelectorAll('.category-button');
+const projectContainers = document.querySelectorAll('.slider-project');
+
+// Add click event listeners to each category button
+categoryButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the category data value for the clicked button
+    const selectedCategory = button.getAttribute('data-category');
+
+    // Hide all project containers
+    projectContainers.forEach(container => {
+      container.style.display = 'none';
+    });
+
+    // Show project containers with matching data category
+    const selectedContainers = document.querySelectorAll(`[data-categories="${selectedCategory}"]`);
+    selectedContainers.forEach(container => {
+      container.style.display = 'flex';
+      container.style.flexDirection = 'column';
+      container.style.justifyContent = 'center';
+    });
+  });
+});
+
